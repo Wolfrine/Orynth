cb185e21e5afd2dc7f64f6d7ba3d2e99fa407fec
Wolfrine <schttewary@gmail.com>
2025-06-29 10:35:26 +0530

    feat(firebase): add production config

---
+++ Orynth/src/app/components/login-upgrade-modal/login-upgrade-modal.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login-upgrade-modal',
+  imports: [CommonModule],
+  templateUrl: './login-upgrade-modal.html'
+})
+export class LoginUpgradeModalComponent {}
--- Orynth/src/app/services/progress/progress.service.ts
+++ Orynth/src/app/services/progress/progress.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Firestore, doc, setDoc } from '@angular/fire/firestore';
+import { AuthService } from '../auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProgressService {
 
-  constructor() { }
+  constructor(private firestore: Firestore, private auth: AuthService) { }
+
+  async setProgress(subjectId: string, data: any) {
+    const uid = this.auth.getCurrentUserId();
+    const ref = doc(this.firestore, `Users/${uid}/Progress/${subjectId}`);
+    await setDoc(ref, data, { merge: true });
+  }
 }
